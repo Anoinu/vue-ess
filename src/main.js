e773c6565a3faf83c10d651bf358fd3d58fe7590
lejin <jin.le@tingshuo.org>
2020-12-14 13:30:33 +0800

    vuejs-ess first time finish

--- src/main.js
+++ src/main.js
@@ -1,8 +1,48 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App'
+import router from './router'
+import './directives'
+import './components'
+import store from './store'
+// 引入插件
+import VueSweetalert2 from './plugins/vue-sweetalert2'
+import Message from './plugins/message'
+import './filters'
+import { mockArticles } from './mock/data'
+import ls from './utils/localStorage'
+import './mock'
+import axios from 'axios'
+
+// 使用插件
+Vue.use(VueSweetalert2)
+Vue.use(Message)
+Vue.prototype.$axios = axios
 
 Vue.config.productionTip = false
 
+const AddMockData = (() => {
+  // 是否加入测试数据
+  const isAddMockData = true
+  // 用户数据
+  let userArticles = ls.getItem('articles')
+
+  if (Array.isArray(userArticles)) {
+    userArticles = userArticles.filter(article => parseInt(article.uid) === 1)
+  } else {
+    userArticles = []
+  }
+
+  if (isAddMockData) {
+    // 合并用户数据和测试数据，使用合并值作为所有文章
+    store.commit('UPDATE_ARTICLES', [...userArticles, ...mockArticles(60)])
+  } else {
+    // 使用用户数据作为所有文章
+    store.commit('UPDATE_ARTICLES', userArticles)
+  }
+})()
+
 new Vue({
+  router,
+  store,
   render: h => h(App),
 }).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,64 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import routes from './routes'
+
+Vue.use(Router)
+
+
+const router =  new Router({
+  mode: 'history',
+  linkExactActiveClass: 'active',
+  scrollBehavior(to, from, savedPosition) {
+    if (to.hash) {
+      // 有锚点时，滚动到锚点
+      return { selector: to.hash }
+    } else if (savedPosition) {
+      // 有保存位置时，滚动到保存位置
+      return savedPosition
+    } else {
+      // 默认滚动到页面顶部
+      return { x: 0, y: 0 }
+    }
+  },
+  routes
+})
+
+// 全局前置守卫
+router.beforeEach((to, from, next) => {
+  const app = router.app
+  const store = app.$options.store
+  const auth = store.state.auth
+  const articleId = to.params.articleId
+  const user = store.state.user && store.state.user.name
+  const paramUser = to.params.user
+
+  app.$message.hide()
+
+  if (
+    (auth && to.path.indexOf('/auth/') !== -1) ||
+    (!auth && to.meta.auth) ||
+    (articleId && !store.getters.getArticleById(articleId)) ||
+    (paramUser && paramUser !== user && !store.getters.getArticlesByUid(null, paramUser).length)
+  ) {
+    next('/')
+  } else {
+    next()
+  }
+})
+
+// 注册全局后置钩子
+router.afterEach((to, from) => {
+  const app = router.app
+  const store = app.$options.store
+  const showMsg = to.params.showMsg
+
+  if (showMsg) {
+    if (typeof showMsg === 'string') {
+      app.$message.show(showMsg)
+    } else {
+      app.$message.show('操作成功')
+    }
+  }
+})
+
+export default router
